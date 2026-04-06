096b11bc6debdb5fd092dbab8ecd46e9fe988815
actualAnian <dajams999@gmail.com>
2026-04-07 00:47:32 +0200

    add a patch to prevent crashes related to vanilla background

---
+++ BloodAndBittersteel/Features/RemoveVanillaLords/BackstoryOnNewGameCreatedPatch.cs
@@ -0,0 +1,14 @@
+﻿using HarmonyLib;
+using TaleWorlds.CampaignSystem.CampaignBehaviors;
+
+namespace BloodAndBittersteel.Features.RemoveVanillaLords
+{
+    [HarmonyPatch(typeof(BackstoryCampaignBehavior), "OnNewGameCreated")]
+    public static class BackstoryOnNewGameCreatedPatch
+    {
+        public static bool Prefix()
+        {
+            return false;
+        }
+    }
+}
