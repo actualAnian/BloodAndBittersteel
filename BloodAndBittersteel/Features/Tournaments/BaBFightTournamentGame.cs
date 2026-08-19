336ff5812544621cbc7669add3225e252c0c82f5
actualAnian <dajams999@gmail.com>
2026-08-19 14:42:04 +0200

    add the rule evaluator to vanilla fight tournaments for custom rewards

---
+++ BloodAndBittersteel/Features/Tournaments/BaBFightTournamentGame.cs
@@ -0,0 +1,16 @@
+﻿using BloodAndBittersteel.Features.Jousting;
+using TaleWorlds.CampaignSystem.Settlements;
+using TaleWorlds.CampaignSystem.TournamentGames;
+using TaleWorlds.Core;
+
+namespace BloodAndBittersteel.Features.Tournaments
+{
+    public class BaBFightTournamentGame : FightTournamentGame
+    {
+        public BaBFightTournamentGame(Town town) : base(town) {}
+        protected override ItemObject GetTournamentPrize(bool includePlayer, int lastRecordedLordCountForTournamentPrize)
+        {
+            return JoustingConfig.GetJoustingRewardItem.Get(Town);
+        }
+    }
+}
---
+++ BloodAndBittersteel/Features/Tournaments/TournamentConfig.cs
@@ -0,0 +1,36 @@
+﻿using BloodAndBittersteel.Library.RuleEngine;
+using System.Collections.Generic;
+using TaleWorlds.CampaignSystem.Settlements;
+using TaleWorlds.Core;
+using TaleWorlds.ObjectSystem;
+
+namespace BloodAndBittersteel.Features.Tournaments
+{
+    public static class TournamentConfig
+    {
+        private readonly static Dictionary<string, List<string>> ClanItems = new()
+        {
+            [Globals.ClanLannisterId] = new() { "grain", "grain" },
+            [Globals.ClanStarkId] = new() { "grain", "grain" },
+        };
+        private readonly static Dictionary<string, List<string>> CultureItems = new()
+        {
+            [Globals.IronbornCultureId] = new() { "grain", "grain" },
+        };
+        private static readonly Rule<Town, ItemObject> BelongsToClan = new(
+            matches: town => ClanItems.ContainsKey(town.OwnerClan.StringId),
+            resolve: town => MBObjectManager.Instance.GetObject<ItemObject>(ClanItems[town.OwnerClan.StringId].GetRandomElement()));
+        private static readonly Rule<Town, ItemObject> BelongsToCulture = new(
+            matches: town => CultureItems.ContainsKey(town.OwnerClan.Culture.StringId),
+            resolve: town => MBObjectManager.Instance.GetObject<ItemObject>(CultureItems[town.OwnerClan.Culture.StringId].GetRandomElement()));
+        private static readonly Rule<Town, ItemObject> Fallback = new(
+            matches: town => true,
+            resolve: town => MBObjectManager.Instance.GetObject<ItemObject>("grain"));
+        public static OrderedRuleEngine<Town, ItemObject> GetBasicTournamentRewardItem = new(new List<Rule<Town, ItemObject>>()
+        {
+            BelongsToClan,
+            BelongsToCulture,
+            Fallback
+        });
+    }
+}
