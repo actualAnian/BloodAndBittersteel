e29f41674798e81c26862dbfdf1cf7bef7499bea
actualAnian <dajams999@gmail.com>
2026-09-26 16:19:54 +0200

    add handling of leftstance animation, fix resetting the equipped weapon on button press, code refactor

--- BloodAndBittersteel/Features/HelmetTilting/AgentExtensions.cs
+++ BloodAndBittersteel/Features/HelmetTilting/AgentExtensions.cs
@@ -9,7 +9,7 @@ namespace BloodAndBittersteel.Features.HelmetTilting
     {
         private static readonly PropertyInfo SpawnEquipmentProperty = typeof(Agent).GetProperty("SpawnEquipment", BindingFlags.Public | BindingFlags.Instance)!;
 
-        public static void RefreshCharacterEquipmentWithoutStoppingAnimation(this Agent agent, Equipment newSpawnEquipment)
+        public static void RefreshCharacterEquipmentKeepAnimationAndWieldedWeapon(this Agent agent, Equipment newSpawnEquipment)
         {
             // forked from Agent.UpdateSpawnEquipmentAndRefreshVisuals
             SpawnEquipmentProperty.SetValue(agent, newSpawnEquipment);
@@ -19,10 +19,22 @@ namespace BloodAndBittersteel.Features.HelmetTilting
                 GameNetwork.WriteMessage(new SynchronizeAgentSpawnEquipment(agent.Index, agent.SpawnEquipment));
                 GameNetwork.EndBroadcastModuleEvent(GameNetwork.EventBroadcastFlags.AddToMissionRecord);
             }
+            ItemObject? wieldedItemBeforeEdit = agent.WieldedWeapon.Item;
             agent.EquipItemsFromSpawnEquipment(true, true, false, 0);
             agent.CheckEquipmentForCapeClothSimulationStateChange();
+            var slotIndex = EquipmentIndex.None;
+            for (EquipmentIndex equipmentIndex = EquipmentIndex.WeaponItemBeginSlot; equipmentIndex < EquipmentIndex.NonWeaponItemBeginSlot; equipmentIndex++)
+            {
+                if (!agent.Equipment[equipmentIndex].IsEmpty && wieldedItemBeforeEdit == agent.Equipment[equipmentIndex].Item)
+                {
+                    slotIndex = equipmentIndex;
+                    break;
+                }
+            }
+            if (slotIndex != EquipmentIndex.None)
+                agent.TryToWieldWeaponInSlot(slotIndex, Agent.WeaponWieldActionType.Instant, true);
             agent.UpdateAgentProperties();
-            //agent.PreloadForRendering(); // 1.5.1 this method stops any current player animation, can not be used
+            //agent.PreloadForRendering(); // 1.5.2 this method stops any current player animation, can not be used
         }
     }
 }
--- BloodAndBittersteel/Features/HelmetTilting/HelmetTiltingMissionLogic.cs
+++ BloodAndBittersteel/Features/HelmetTilting/HelmetTiltingMissionLogic.cs
@@ -1,5 +1,6 @@
 ﻿using BloodAndBittersteel.MCM;
 using System;
+using System.Linq;
 using System.Threading.Tasks;
 using TaleWorlds.Core;
 using TaleWorlds.InputSystem;
@@ -10,22 +11,48 @@ namespace BloodAndBittersteel.Features.HelmetTilting
 {
     public class HelmetTiltingMissionLogic : MissionLogic
     {
+        private readonly static string CanNotSwapText = "{=bab_can_not_tilt_helmet}You need to put your shield away first";
+        private bool HasWeaponInLeftHand(Agent agent)
+        {
+            return agent.WieldedOffhandWeapon.Item != null;
+        }
         public override void OnMissionTick(float dt)
         {
-            if (Input.IsKeyPressed(BaBSettings.Instance.HelmetTilting))
+            Agent agent = Agent.Main;
+
+            if (agent == null || !Input.IsKeyPressed(BaBSettings.Instance.HelmetTilting))
+                return;
+            ToggleHelmetVisor(agent);
+        }
+        private void ToggleHelmetVisor(Agent agent)
+        {
+            bool isLeftStance = agent.GetIsLeftStance();
+            foreach (EquipmentIndex equipmentIndex in Enum.GetValues(typeof(EquipmentIndex)).Cast<EquipmentIndex>().Where(index => (int)index >= 0 && (int)index < 12))
             {
-                if (Agent.Main == null) return;
-                foreach (EquipmentIndex eqIndex in Enum.GetValues(typeof(EquipmentIndex)))
+                if (agent.SpawnEquipment[equipmentIndex].IsEmpty)
+                    continue;
+                string currentItemId = agent.SpawnEquipment[equipmentIndex].Item.StringId;
+                foreach (var swap in HelmetSwapManager.Instance.Swaps)
                 {
-                    if ((int)eqIndex >= 12 || (int)eqIndex < 0) continue;
-                    if (Agent.Main.SpawnEquipment[eqIndex].IsEmpty) continue;
-                    foreach (var swap in HelmetSwapManager.Instance.Swaps)
+                    if (currentItemId == swap.VisorOpenedItemId)
                     {
-                        string currentId = Agent.Main.SpawnEquipment[eqIndex].Item.StringId;
-                        if (currentId == swap.VisorOpenedItemId)
-                            SwapItem(eqIndex, swap.VisorClosedItemId, "act_visor_close");
-                        else if (currentId == swap.VisorClosedItemId)
-                            SwapItem(eqIndex, swap.VisorOpenedItemId, "act_visor_open");
+                        if (HasWeaponInLeftHand(agent))
+                        {
+                            MBInformationManager.AddQuickInformation(new(CanNotSwapText));
+                            return;
+                        }
+                        SwapItem(equipmentIndex, swap.VisorClosedItemId, ChooseAnimation(isLeftStance, false));
+                        return;
+                    }
+                    if (currentItemId == swap.VisorClosedItemId)
+                    {
+                        if (HasWeaponInLeftHand(agent))
+                        {
+                            MBInformationManager.AddQuickInformation(new(CanNotSwapText));
+                            return;
+                        }
+                        SwapItem(equipmentIndex, swap.VisorOpenedItemId, ChooseAnimation(isLeftStance, true));
+                        return;
                     }
                 }
             }
@@ -40,13 +67,27 @@ namespace BloodAndBittersteel.Features.HelmetTilting
                     newEquipment[i] = new EquipmentElement(Agent.Main.Equipment[i].Item);
             }
             newEquipment[index] = new EquipmentElement(newItem);
-            Agent.Main.RefreshCharacterEquipmentWithoutStoppingAnimation(newEquipment);
+            Agent.Main.RefreshCharacterEquipmentKeepAnimationAndWieldedWeapon(newEquipment);
         }
         private void SwapItem(EquipmentIndex index, string newItemId, string animationAction)
         {
             var newItem = MBObjectManager.Instance.GetObject<ItemObject>(newItemId);
             Agent.Main.SetActionChannel(0, ActionIndexCache.Create(animationAction), true);
             ChangePlayerHelmetWithDelay(index, newItem);
+        }
+        private string ChooseAnimation(bool isLeftStance, bool isOpeningVisor)
+        {
+            switch (isLeftStance, isOpeningVisor)
+            {
+                case (true, true):
+                    return "act_visor_open_leftstance";
+                case (true, false):
+                    return "act_visor_close_leftstance";
+                case (false, true):
+                    return "act_visor_open";
+                case (false, false):
+                    return "act_visor_close";
+            }
         }
     }
 }
